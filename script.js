60d91ce0eb64adec8dca6984b8511f330f18662c
Nipan  Debnath <113585057+nath-Nipan-2022@users.noreply.github.com>
2023-01-10 00:35:02 +0530

    Green Website Demo deploy

---
+++ script.js
@@ -0,0 +1,12 @@
+// selecting the nav
+const nav = document.querySelector("nav");
+const navLinks = document.querySelector(".nav-links");
+// selecting the menu
+const menu = document.querySelector("#menu");
+
+menu.addEventListener("click", function (e) {
+	e.stopPropagation();
+	menu.classList.toggle("active");
+	nav.classList.toggle("active");
+	navLinks.classList.toggle("active");
+});
